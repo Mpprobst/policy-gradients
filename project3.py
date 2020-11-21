1d9250f5678b889b35e5ab1d20293e89a91d07af
Michael Probst <mpprobst@gmail.com>
2020-11-21 13:44:32 -0500

    Clean Stuff up

--- project3.py
+++ project3.py
@@ -13,18 +13,12 @@ import randomAgent
 ENVIRONMENTS_MAP = {'cartpole' : cartpole.CartPole,
                     'lunarlander' : lunarlander.LunarLander }
 
-# might add an A3C agent later on
-AGENTS_MAP = {'random' : randomAgent.RandomAgent,
-              'REINFORCE' : reinforce.ReinforceAgent }
-
 parser = argparse.ArgumentParser(description='Define the problem to solve.')
-parser.add_argument('--agent', choices=AGENTS_MAP.keys(), default='random', help='Can be random, or REINFORCE')
 parser.add_argument('--env', choices=ENVIRONMENTS_MAP.keys(), default='cartpole', help='Can be cartpole or lunarlander')
-parser.add_argument('--numEpisodes', type=int, default = 10, help='Number of episodes you want the agent to run.')
-parser.add_argument('--verbose', help='Print more information.', action='store_true')
+parser.add_argument('--numEpisodes', type=int, default = 500, help='Number of episodes you want the agent to run.')
+parser.add_argument('--verbose', help='Visualize the environment.', action='store_true')
 args = parser.parse_args()
 
-agentFunc = AGENTS_MAP[args.agent]
 envFunc = ENVIRONMENTS_MAP[args.env]
 
-env = envFunc(args.numEpisodes, agentFunc, args.verbose)
+env = envFunc(args.numEpisodes, reinforce.ReinforceAgent, args.verbose)
